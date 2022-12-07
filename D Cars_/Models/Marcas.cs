0e8a495a0ceee4fc2de8cd2cea102a635a8bdf49
david quintanilla <davidquintanilla180@gmail.com>
2022-12-07 09:17:17 -0600

    Agregar archivos de proyecto.

---
+++ D Cars_/Models/Marcas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_Cars_.Models
+{
+    public class Marcas
+    {
+        public string Name { get; set; }
+        public decimal  Precie { get; set; }
+        public string Tipo_producto { get; set; }
+
+
+
+    }
+}
---
+++ D Cars_/Models/Productos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_Cars_.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Precio { get; set; }
+        public string Marca { get; set; }
+    }
+}
